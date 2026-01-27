af9b534e8d14678e7f84ae8bf78e61290678d057
Heart Anana <2023_cete_ananahea@online.htcgsc.edu.ph>
2026-01-27 11:16:36 +0800

    feat: Add SSL config for cloud database

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // This loads the variables from .env
+require('dotenv').config();
+
+// --- NEW LOGIC TO HANDLE SSL ---
+const sslConfig = process.env.DB_SSL_REQUIRED === 'true' 
+  ? { ssl: { rejectUnauthorized: false } } 
+  : {};
+// ---------------------------------
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT, // <-- Make sure this is here
   database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ...sslConfig // <-- Add the SSL config here
 });
 
-// We export the promise-based version of the pool
 module.exports = pool.promise();
