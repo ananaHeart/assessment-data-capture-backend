e694de77f329ae9256c162384e2c3754bd66884d
Heart Anana <2023_cete_ananahea@online.htcgsc.edu.ph>
2026-03-04 11:18:54 +0800

    Fix: Sync database environment variables and API URL

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT, // <-- Make sure this is here
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
