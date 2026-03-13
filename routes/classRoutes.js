04615475c29090ef13d2532389d7525c54b221b6
Heart Anana <2023_cete_ananahea@online.htcgsc.edu.ph>
2026-03-13 20:00:54 +0800

    Initial backend commit

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createClass, getAllClasses, getMyClasses } = require('../controllers/classController');
-const { protect, restrictTo } = require('../middleware/authMiddleware');
 
-// --- Principal/Admin Routes ---
-// protect -> make sure user is logged in
-// restrictTo('principal', 'admin') -> make sure user is a principal or admin
-router.post('/', protect, restrictTo('principal', 'admin'), createClass);
-router.get('/', protect, restrictTo('principal', 'admin'), getAllClasses);
+const {
+  createClass,
+  getAllClasses,
+  getMyClasses,
+  getStudentsByClass,
+  getAssessmentsByClass
+} = require("../controllers/classController");
 
-// --- Teacher Route ---
-// A new function getMyClasses will be added to classController
-router.get('/my-classes', protect, restrictTo('teacher'), getMyClasses);
+const { protect, restrictTo } = require("../middleware/authMiddleware");
+
+router.post("/", protect, restrictTo("admin"), createClass);
+
+router.get("/", protect, getAllClasses);
+
+router.get("/my-classes", protect, restrictTo("teacher"), getMyClasses);
+
+router.get("/:classId/students", protect, restrictTo("teacher"), getStudentsByClass);
+
+router.get("/:classId/assessments", protect, restrictTo("teacher"), getAssessmentsByClass);
 
 module.exports = router;
